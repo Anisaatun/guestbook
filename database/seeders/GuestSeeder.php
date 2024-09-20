ed38b131b7c4442f6456449c9d1e7e529b8b3c38
nur anisa <nrnsa4560@gmail.com>
2024-09-20 09:53:32 +0700

    membuat guest form

--- database/seeders/GuestSeeder.php
+++ database/seeders/GuestSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Guest;
+use App\Models\Institution;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,13 +14,10 @@ class GuestSeeder extends Seeder
      */
     public function run(): void
     {
-        Guest::create([
-            'fullname' => 'Anisa',
-            'institution_id' => 3,
-            'from' => 'PT Bim',
-            'phonenumber' => '08123456789',
-            'email' => 'Anisa@mail.com',
-            'note' => 'Test',
-        ]);
+        Institution::create(['name' => 'perusahaan']);
+        Institution::create(['name' => 'Dinas']);
+        Institution::create(['name' => 'Sekolah']);
+
+        Guest::factory(1000)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\FormGuestController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FormGuestController::class, 'index'])->name('form.index');
+Route::post('/', [FormGuestController::class, 'store'])->name('form.store');
 
 Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);
 
@@ -17,5 +17,7 @@ Route::group(['middleware' => ['auth'], 'prefix' => 'admin', 'as' => 'admin.'], 
     Route::resource('/institution', App\Http\Controllers\InstitutionController::class);
 
     Route::resource('/guests', App\Http\Controllers\GuestController::class)->only(['index', 'show', 'destroy']);
+
+    Route::get('/reports', [App\Http\Controllers\ReportController::class, 'index'])->name('reports.index');
 });
 
